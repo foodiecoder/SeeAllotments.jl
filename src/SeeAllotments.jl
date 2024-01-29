cbe051108c6dd0a6b141e6f5a1033b5c84ffdae4
Thura Z <inet.mg@gmail.com>
2024-01-29 02:30:11 +0000

    Data Structure added.

--- src/SeeAllotments.jl
+++ src/SeeAllotments.jl
@@ -1,5 +1,21 @@
 module SeeAllotments
 
-# Write your package code here.
+greet() = print("Hey ChatGPT!")
 
-end
+# Use preferences
+export set_town
+
+# Location
+export where_am_i
+
+# Allotments directory
+export loc_allotment, site_info, contact_info
+
+# Historical weather data
+export plot_hist_temp, plot_hist_rain, plot_hist_snow
+export plot_hist_humidity, plot_hist_windspeed, plot_hist_solar
+
+# Export data
+export export_to_sqlite
+
+end # module
